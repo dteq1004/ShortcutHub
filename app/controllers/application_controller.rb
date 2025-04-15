c5afb1b90cc4b6b48d0dad6839008480ea8f4a68
dai <dai.shohs2064@gmail.com>
2025-04-15 10:37:12 +0900

    ✨ Add：新規登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_header_visibility
+
+  private
+
+  def set_header_visibility
+    @show_header = true # デフォルトはヘッダーを表示する
+  end
 end
