c22198b0f5eb115d3c90b80cac75358c1be55dab
dai <dai.shohs2064@gmail.com>
2025-04-16 16:17:49 +0900

    ✨ Add：ユーザー名とユーザーIDの編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:mypage]
+  before_action :authenticate_user!, except: [:show]
+  before_action :ensure_user, only: [:edit, :update, :destroy]
 
   def show
     @user = User.find_by(uid: params[:uid])
@@ -10,7 +11,36 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to mypage_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy!
+    redirect_to root_path
+  end
+
   def mypage
     redirect_to user_path(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :uid)
+  end
+
+  def ensure_user
+    @user = User.find_by(uid: params[:uid])
+    if @user != current_user
+      redirect_to user_path(@user)
+    end
   end
 end
