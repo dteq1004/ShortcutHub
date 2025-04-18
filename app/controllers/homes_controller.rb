923f049d85d78d3c18e3b0b236aadb54fa92758e
dai <dai.shohs2064@gmail.com>
2025-04-18 20:02:51 +0900

    ✨ Add：公式のショートカット

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
   def index
     @shortcuts = Shortcut.includes(:user).where(status: :published).order(created_at: :desc)
+    @official_shortcuts = OFFICIAL_SHORTCUTS
   end
 end
---
+++ config/initializers/official_shortcuts.rb
@@ -0,0 +1,1 @@
+OFFICIAL_SHORTCUTS = YAML.load_file(Rails.root.join("config", "official_shortcuts.yml"))
