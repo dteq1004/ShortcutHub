5fdccdc38982301a6d19e315aa46f129a063a450
dai <dai.shohs2064@gmail.com>
2025-04-21 11:23:01 +0900

    ✨ Add：ショートカットのアイコン設定機能

--- app/javascript/edit_shortcut.js
+++ app/javascript/edit_shortcut.js
@@ -1,42 +1,38 @@
-const openPublishedModalButton = document.getElementById("openPublishedModal");
-const closePublishedModalButton = document.getElementById("closePublishedModal");
-const publishedModal = document.getElementById("published_modal");
-const statusInput = document.getElementById("shortcut_status");
-
-const changeStatusToPublished = () => {
-    statusInput.value = "published";
-}
-const changeStatusToDraft = () => {
-    statusInput.value = "draft";
-}
-
-openPublishedModalButton.addEventListener("click", () => {
-    publishedModal.showModal();
-    changeStatusToPublished();
-});
-closePublishedModalButton.addEventListener("click", () => {
-    publishedModal.close();
-    changeStatusToDraft();
-});
-publishedModal.addEventListener("click", (event) => {
-    if (event.target.closest("#publishedModalContainer") === null ) {
-        publishedModal.close();
-        changeStatusToDraft();
-    }
-});
-
-document.querySelectorAll(".auto-adjust").forEach((targetArea) => {
-    let lineHeight = Number(targetArea.rows);
-    while(targetArea.scrollHeight > targetArea.offsetHeight) {
-        lineHeight++;
-        targetArea.rows = lineHeight;
-    }
-
-    targetArea.addEventListener("input", (e) => {
-        e.target.style.height = 0
-        e.target.style.height = e.target.scrollHeight + "px"
-        if(e.target.offsetHeight < 40){
-            e.target.style.height = 40 + "px"
+document.addEventListener("turbo:load", function() {
+    document.querySelectorAll(".auto-adjust").forEach((targetArea) => {
+        let lineHeight = Number(targetArea.rows);
+        while(targetArea.scrollHeight > targetArea.offsetHeight) {
+            lineHeight++;
+            targetArea.rows = lineHeight;
         }
+        targetArea.addEventListener("input", (e) => {
+            e.target.style.height = 0
+            e.target.style.height = e.target.scrollHeight + "px"
+            if(e.target.offsetHeight < 40){
+                e.target.style.height = 40 + "px"
+            }
+        })
     })
+    const iconPreview = document.querySelector('#icon_preview')
+    const iconBtn = document.querySelector('#icon_select')
+    const iconModal = document.querySelector('#icon_modal')
+    iconBtn.addEventListener('click', () => {
+        iconModal.showModal()
+    })
+    iconModal.addEventListener('click', (event) => {
+        if (event.target.closest('#icon_modal_container') === null ) {
+            iconModal.close();
+        }
+    });
+    for (let step = 1; step < 42; step++ ) {
+        document.querySelector(`#icon${step}`).addEventListener('click', (e) => {
+            const img = document.createElement('img')
+            const oldImg = iconPreview.firstElementChild
+            const input = document.querySelector('#shortcut_icon')
+            img.setAttribute('src', e.target.attributes.src.nodeValue)
+            iconPreview.replaceChild(img, oldImg)
+            input.setAttribute('value', `icon${step}`)
+            iconModal.close();
+        })
+    }
 })
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_16_074219) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_18_121857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_16_074219) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
+    t.string "icon", default: "icon1"
     t.index ["user_id"], name: "index_shortcuts_on_user_id"
   end
 
