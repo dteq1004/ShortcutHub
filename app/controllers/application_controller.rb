52d10b96f74c20b1ce78454f97784b5728db28f3
dai <dai.shohs2064@gmail.com>
2025-05-01 17:49:05 +0900

    🩹 Fix：軽微な修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :set_header_visibility
 
+  def after_register_path_for(resource)
+    root_path
+  end
+
   private
 
   def set_header_visibility
