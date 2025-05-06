7df782ce92e31ae9a2b2f886cd94da81b8067bb7
dai <dai.shohs2064@gmail.com>
2025-05-06 22:20:31 +0900

    ✨ Add：コメントのajax化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,6 @@ class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
     @comment.save
-    redirect_to request.referer
   end
 
   def update
