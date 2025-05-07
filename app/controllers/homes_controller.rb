85c0c73de7454a2cbd9e4ec0b97a60831730fa82
dai <dai.shohs2064@gmail.com>
2025-05-07 15:37:43 +0900

    ✨ Add：ショートカットの人気ランキング

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,14 @@ class HomesController < ApplicationController
   end
 
   def index_lazy
-    @shortcuts = Shortcut.includes(:user).where(status: :published).order(created_at: :desc).limit(10)
+    @latest_shortcuts = Shortcut.includes(:user).where(status: :published).order(created_at: :desc).limit(10)
+    @popular_shortcuts = Shortcut
+      .includes(:user)
+      .where(status: :published)
+      .select('shortcuts.*,
+        COALESCE(view_count, 0) + COALESCE((SELECT COUNT(*) FROM bookmarks WHERE bookmarks.shortcut_id = shortcuts.id), 0) * 10 + COALESCE((SELECT COUNT(*) FROM favorites WHERE favorites.shortcut_id = shortcuts.id), 0) * 5 AS total_count')
+      .order('total_count DESC')
+      .limit(10)
     @official_shortcuts = OFFICIAL_SHORTCUTS
     @news = News.page(offset: 0, limit: 3)
   end
