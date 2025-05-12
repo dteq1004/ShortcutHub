9e25b2f88f9957900f86352e66583238da8994ca
dai <dai.shohs2064@gmail.com>
2025-05-12 14:10:25 +0900

    📝 Add：公式のショートカットの追加

--- app/javascript/controllers/title_count_controller.js
+++ app/javascript/controllers/title_count_controller.js
@@ -2,24 +2,39 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="title-count"
 export default class extends Controller {
-  static targets = [ "output", "field" ]
-
-  static values = {
-    characterCount: Number
-  }
+  static targets = [ "count", "title", "error_title", "submit" ]
 
   connect() {
     this.change()
   }
 
+  static values = {
+    characterCount: Number
+  }
+
   change() {
-    let length = this.fieldTarget.value.length
-    this.outputTargets[0].textContent = `${length} / 50`
+    const titleInput = this.titleTarget
+    const titleError = this.error_titleTarget
+    const submitBtn = this.submitTarget
+    let length = titleInput.value.length
+    this.countTarget.textContent = `${length} / 50`
 
     if (length < 3 || length > this.characterCount) {
-      this.outputTargets[0].classList.add("text-rose-500")
+      this.countTarget.classList.add("text-rose-500")
+      titleInput.classList.remove("border-zinc-300", "focus:border-zinc-500", "border-teal-500", "focus:border-teal-500")
+      titleInput.classList.add("border-rose-500", "focus:border-rose-500")
+      titleError.textContent = "3文字以上50文字以内で入力してください。"
+      submitBtn.classList.remove("bg-teal-500")
+      submitBtn.classList.add("bg-teal-500/50")
+      submitBtn.disabled = true
     } else {
-      this.outputTargets[0].classList.remove("text-rose-500")
+      this.countTarget.classList.remove("text-rose-500")
+      titleInput.classList.remove("border-rose-500", "focus:border-rose-500", "border-zinc-300", "focus:border-zinc-500")
+      titleInput.classList.add("border-teal-500", "focus:border-teal-500")
+      titleError.textContent = ""
+      submitBtn.classList.remove("bg-teal-500/50")
+      submitBtn.classList.add("bg-teal-500")
+      submitBtn.disabled = false
     }
   }
 }
