78c2ed6c3e0d7dca6a97aa79dd8986be87a29d3f
dai <dai.shohs2064@gmail.com>
2025-05-20 15:38:26 +0900

    ✨ Add：検索のオートコンプリート機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,12 +5,21 @@ class SearchesController < ApplicationController
     @shortcuts = if (tag_name = params[:tag_name])
       @q.result(distinct: true).includes(:user).joins(:tags).where(status: :published).where(tags: { name: tag_name }).order(created_at: :desc)
     else
-      @q.result(distinct: true).includes(:user).joins(:tags).where(status: :published).order(created_at: :desc)
+      @q.result(distinct: true).includes(:user).where(status: :published).order(created_at: :desc)
     end
   end
 
   def search
     @tags = Tag.where(id: Tagging.group(:tag_id).order("count_tag_id desc").limit(10).count(:tag_id).keys)
+  end
+
+  def autocomplete
+    unless request.format.json?
+      redirect_to root_path and return
+    end
+    q = Shortcut.ransack(title_or_description_cont: params[:q])
+    shortcuts = q.result(distinct: true).where(status: :published).limit(5).pluck(:title).uniq
+    render json: shortcuts
   end
 
   private
