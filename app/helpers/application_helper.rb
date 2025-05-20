30295ab476a8496f99d12ce1cac1ce2dd9647bf4
dai <dai.shohs2064@gmail.com>
2025-05-20 12:01:53 +0900

    ✨ 静的OGP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,30 @@ module ApplicationHelper
     else "bg-gray-500"
     end
   end
+
+  def default_meta_tags
+    {
+      site: "Shortcut Hub（ショートカットハブ）",
+      title: "iPhoneのショートカットのレシピを共有できるプラットフォーム",
+      reverse: true,
+      charset: "utf-8",
+      description: "Shortcut Hub（ショートカットハブ）ではiPhoneをもっと便利にするためのショートカットのレシピを共有できるプラットフォームです。",
+      keywords: "iPhone,ショートカット",
+      canonical: request.original_url,
+      separator: '|',
+      og:{
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url("ogp.png"),
+        local: "ja-JP"
+      },
+      twitter: {
+        card: 'summary_large_image',
+        image: image_url('ogp.png')
+      }
+    }
+  end
 end
