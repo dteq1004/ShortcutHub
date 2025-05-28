2294e59b9d173ec0887620996e32cf0a72440ae5
dai <dai.shohs2064@gmail.com>
2025-05-28 10:15:04 +0900

    ✨ Add：Xシェア機能

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -1,6 +1,14 @@
 class Instruction < ApplicationRecord
+  before_validation :sanitize_instruction, on: :update
+
   validates :step_number, presence: :true
   validates :content, presence: :true
 
   belongs_to :shortcut
+
+  private
+
+  def sanitize_instruction
+    self.instruction = ActionController::Base.helpers.sanitize(instruction, tags: [], attributes: [])
+  end
 end
